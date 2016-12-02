4d8c2c3ea0dc044cd274dce752a6eb4583c51f9a
Ryan Vo <ryanvo@seas.upenn.edu>
2016-12-01 22:13:52 -0500

    tdd support

---
+++ cit-591-recitation-9/test/twitter/TweetCleanerTest.java
@@ -0,0 +1,29 @@
+package twitter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TweetCleanerTest {
+    @Test
+    public void shouldDoThis() throws Exception {
+
+
+
+    }
+
+    @Test
+    public void shouldAlsoDoThat() throws Exception {
+
+
+
+    }
+
+    @Test
+    public void shouldNeverDoThis() throws Exception {
+
+
+
+    }
+
+}
---
+++ cit-591-recitation-9/test/watson/WatsonJsonParserTest.java
@@ -0,0 +1,28 @@
+package watson;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WatsonJsonParserTest {
+    @Test
+    public void shouldDoThis() throws Exception {
+
+
+
+    }
+
+    @Test
+    public void shouldAlsoDoThat() throws Exception {
+
+
+
+    }
+
+    @Test
+    public void shouldNeverDoThis() throws Exception {
+
+
+
+    }
+}
